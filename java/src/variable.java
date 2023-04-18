98bd190110655640fc7824508a30c1dd99ed8e06
hmduc <ducgiang1905@gmail.com>
2023-04-18 17:33:17 +0700

    update

---
+++ java/src/variable.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class variable {
+    public void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr2 = {1, 2, 3, 4, 5};
+    }
+
+    private String[] findStr(String[] strArr, String pattern ) {
+     String[] strs = new String[strArr.length];
+
+    }
+
+
+}
